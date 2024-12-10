a227e431c92d4f0f27b874f0810028e8f9e9b206
Bright <brightagu2@gmail.com>
2024-12-11 00:00:45 +0100

    update

--- app/user/page.js
+++ app/user/page.js
@@ -371,7 +371,7 @@ export default function Home() {
                       >
                         Trending{" "}
                       </Typography>
-                      <TradingViewWidget />
+                       <TradingViewWidget /> 
                     </Box>
                   </Box>
                 </Box>
