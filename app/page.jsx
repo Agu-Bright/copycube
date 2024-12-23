14eb88d217c74a613de948f24a8b2e9377baa85a
Bright <brightagu2@gmail.com>
2024-12-23 07:23:31 +0100

    update

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,13 @@
 "use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Welcome to the App!</h1>
-      <p>This is a placeholder for the root route.</p>
-    </div>
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/home");
+  }, [router]);
+
+  return null;
 }
